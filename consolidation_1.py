8ec9e5d8413ce769020a43cc27acc32d5f463362
Olivia Fulop <ofulop@terpmail.umd.edu>
2024-11-21 11:46:26 -0500

    code comments, add roll score to player score, elif statment to not reroll

--- consolidation_1.py
+++ consolidation_1.py
@@ -1,40 +1,58 @@
-#after each roll, display score
+#import random
 import random
 
-score = 0
-score += 0
+#create a score variable
+#set it equal to zero and add the values of each roll
+player_1_score = 0
 
+#tuple for dice values
 dice_values = (1,2,3,4,5,6)
-random.choices(dice_values)
+
+#three initial rolls
+#print each value for player
+roll_1 = random.choice(dice_values)
+player_1_score += roll_1
+print(f"Player 1 rolled a {roll_1}, for a total score of {player_1_score}.")
+
+roll_2 = random.choice(dice_values)
+player_1_score += roll_2
+print(f"Player 1 rolled a {roll_2}, for a total score of {player_1_score}.")
 
 
-roll_1 = random.choices(dice_values)
-print(roll_1)
+roll_3 = random.choice(dice_values)
+player_1_score += roll_3
+print(f"Player 1 rolled a {roll_3}, for a total score of {player_1_score}.")
 
-roll_2 = random.choices(dice_values)
-print(roll_2)
+#player_1_score = roll_1 + roll_2 + roll_3
+#print(player_1_score)
 
-roll_3 = random.choices(dice_values)
-print(roll_3)
 
-player_1_score = roll_1 + roll_2 + roll_3
-print(player_1_score)
-
+#create if statements to check if any of the roll values equal each other
+#if the values do equal each other, player should have the option to reroll
 if roll_1 == roll_2:
-    reroll_3rd = input("Do you want to reroll your 3rd die? Type 'yes' or 'no'")
-    if reroll_3rd == "yes":
+    reroll_3_Q = input("Do you want to reroll your 3rd die? Type 'yes' or 'no'")
+    if reroll_3_Q == "yes":
          roll_3 = (random.randint(1,6)) 
          print(roll_3)
+    elif reroll_3_Q == "no":
+        pass
+    else: input("Do you want to reroll your 3rd die? Type 'yes' or 'no'")
 elif roll_2 == roll_3:
-    reroll_1st = input("Do you want to reroll your 1st die? Type 'yes' or 'no'\n")
-    if reroll_1st == "yes":
+    reroll_1_Q = input("Do you want to reroll your 1st die? Type 'yes' or 'no'\n")
+    if reroll_1_Q == "yes":
         roll_1 = (random.randint(1,6)) 
         print(roll_1)
+    elif reroll_1_Q == "no":
+        pass
+    else: input("Do you want to reroll your 3rd die? Type 'yes' or 'no'")
 elif roll_1 == roll_3:
-    reroll_2nd = input("Do you want to reroll your 2nd die? Type 'yes' or 'no'")
-    if reroll_2nd == "yes":
+    reroll_2_Q = input("Do you want to reroll your 2nd die? Type 'yes' or 'no'")
+    if reroll_2_Q == "yes":
         roll_2 = (random.randint(1,6)) 
         print(roll_2)
+    elif reroll_2_Q == "no":
+        pass
+    else: input("Do you want to reroll your 3rd die? Type 'yes' or 'no'")
 elif roll_1 == roll_2 == roll_3:
     number = player_1_score
     result = player_1_score + 0
@@ -57,7 +75,7 @@ print(f"Player 1's Score is now {player_1_score}")
     #fixed == True
     #print("It's now player 2's turn")
 
-Score = {"player_1_scores" : score }
+Score = {"player_1_scores" : player_1_score}
                  
-for score in Score["score"]:
+for player_1_score in Score["score"]:
     player_1_score.append(player_1_score)
