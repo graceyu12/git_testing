4dfa36d767bd7ba4332395a86cb160c99d5c8170
Olivia Fulop <ofulop@terpmail.umd.edu>
2024-11-26 16:38:16 -0500

    adding print statements after rerolls

--- consolidation_1.py
+++ consolidation_1.py
@@ -49,18 +49,21 @@ while game_finished == False:
             print(roll_4)
             rolls += 1
             Score['reroll_1'] = roll_4
+            (f"Player 1 rolled a {roll_4}, for a total score of {player_1_score}.")
             reroll_3_Q = input("Do you want to reroll your 3rd die? Type 'yes' or 'no'\n")
             if reroll_3_Q == "yes":
                 roll_5 = random.choice(dice_values)
                 print(roll_5)
                 rolls += 1
                 Score['reroll_2'] = roll_5
+                (f"Player 1 rolled a {roll_5}, for a total score of {player_1_score}.")
                 reroll_3_Q = input("Do you want to reroll your 3rd die? Type 'yes' or 'no'\n")
                 if reroll_3_Q == "yes":
                     roll_6 = random.choice(dice_values)
                     print(roll_6)
                     rolls += 1
                     Score['reroll_3'] = roll_6
+                    (f"Player 1 rolled a {roll_6}, for a total score of {player_1_score}.")
             if rolls >= 6:
                     game_finished = True
         elif reroll_3_Q == "no":
